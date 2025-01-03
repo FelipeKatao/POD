cc221854084ebc753f31030d365f124654373f56
FelipeKatao <cataofelipe@gmail.com>
2025-01-03 20:28:24 -0300

    Create Data Funcitons

--- OrientedData.py
+++ OrientedData.py
@@ -4,5 +4,8 @@ class OrientedData:
     def __init__(self):
         self.Rules = []
 
-    def CreateNewRule(self):
-        pass
+    def CreateNewRule(self,RuleOd:str,fileOd:str):
+        with open(fileOd,'r') as file:
+            data = json.load(file)
+        self.Rules.append(data["Rules"][RuleOd])
+        
